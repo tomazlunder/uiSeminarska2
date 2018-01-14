8beee1e97c47ad2941d52ba592abe73b71c201e9
Klementina Garbajs <klementina.garbajs@gmail.com>
2018-01-14 21:00:20 +0100

    Vektor dolzine 14

--- skripte/customRLs/Klementina_RL1.R
+++ skripte/customRLs/Klementina_RL1.R
@@ -31,39 +31,43 @@ getStateDesc <- function(simData, preyId)
 	# 4. Gozd (index 6)
 	# Border (index 8)
 	# HungerLevel (index 9)
-	vec <- c(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+	vec <- c(
+	  0, 0, 0, 0, 
+	  0, 0, 0, 0, 
+	  0, 0, 0, 0,
+	  0, 0)
 	
 	# PLENILEC
 	res <- getPreyDistAndDirectionToNearestPredator(simData, preyId)
 	distance <- max(res[1], 1) 
-	distance <- min(distance, 30)
+	distance <- min(distance, 10)
 	direction <- res[2]
-	vec[0] <- distance
-	vec[1] <- direction
+	vec[1] <- distance
+	vec[2] <- direction
 	
 	# VODA
 	res <- getPreyDistAndDirectionToNearestWater(simData, preyId)
 	distance <- max(res[1], 1) 
-	distance <- min(distance, 30)
+	distance <- min(distance, 10)
 	direction <- res[2]
-	vec[2] <- distance
-	vec[3] <- direction
+	vec[3] <- distance
+	vec[4] <- direction
 	
 	# TRAVA
 	res <- getPreyDistAndDirectionToNearestGrass(simData, preyId)
 	distance <- max(res[1], 1) 
-	distance <- min(distance, 30)
+	distance <- min(distance, 10)
 	direction <- res[2]
-	vec[4] <- distance
-	vec[5] <- direction
+	vec[5] <- distance
+	vec[6] <- direction
 	
 	# GOZD
 	res <- getPreyDistAndDirectionToNearestForest(simData, preyId)
 	distance <- max(res[1], 1) 
-	distance <- min(distance, 30)
+	distance <- min(distance, 10)
 	direction <- res[2]
-	vec[6] <- distance
-	vec[7] <- direction
+	vec[7] <- distance
+	vec[8] <- direction
 	
 	if (pos[2] == 1)
 	  border <- 1
@@ -76,9 +80,21 @@ getStateDesc <- function(simData, preyId)
 	else
 	  border <- 5
 	
-	vec[8] <- border
-	vec[9] <- preyId
-	vec[10] <- simData
+	vec[9] <- border
+	vec[10] <- preyId
+	
+	if(isPreyOnDirt(simData, preyId))
+	  vec[11] <- 0
+	else if(isPreyOnGrass(simData, preyId))
+	  vec[11] <- 1
+	else if(isPreyInForest(simData, preyId))
+	  vec[11] <- 2
+	else if(isPreyInWater(simData, preyId))
+	  vec[11] <- 3
+	
+	vec[12] <- if(isPreyHidden(simData, preyId)) 0 else 1
+	vec[13] <- if(isPreyThirsty(simData, preyId)) 0 else 1
+	vec[14] <- if(isPreyHungry(simData, preyId)) 0 else 1
 	
 	vec
 }
@@ -93,67 +109,60 @@ getStateDesc <- function(simData, preyId)
 
 getReward <- function(oldstate, action, newstate)
 {
-  preyId <- newstate[9]
-  simData <- newstate[10]
+  preyId <- newstate[10]
 
   # PREMIK PROTI PREDATORJU  > 0: dlje ; < 0: bližje
-  predator_reward = newstate[1] - oldstate[1]
+  predator_reward = newstate[2] - oldstate[2]
   reward <- reward + predator_reward
   
   # ODDALJENOST OD PREDATORJA
-  predator_dist <- newstate[1] - 30
+  predator_dist <- newstate[2] - 10
   reward <- reward + predator_dist
   
   # AGENT NI VIDEN
-  if(isPreyHidden(simData, preyId))
+  if(newstate[12] == 0)
     reward <- reward + 5
   
   # TEREN
-  if(isPreyOnDirt(simData, preyId)) # AGENT NA ZEMLJI
+  teren <- newstate[11]
+  if(teren == 0) # AGENT NA ZEMLJI
     reward <- reward + 1
-  else if(isPreyOnGrass(simData, preyId)) # AGENT NA TRAVI
+  else if(teren == 1) # AGENT NA TRAVI
     reward <- reward - 1
-  else if(isPreyInForest(simData, preyId)) # AGENT V GOZDU
+  else if(teren == 2) # AGENT V GOZDU
     reward <- reward - 2
-  else if(isPreyInWater(simData, preyId)) # AGENT V VODI
+  else # AGENT V VODI
     reward <- reward - 3
   
   # AGENT NI NA MEJI
-  if(newstate[8] == 5)
+  if(newstate[9] == 5)
     reward <- reward + 1
   else if(action < 5) # AGENT NA MEJI IN NE JE/NE PIJE
   {
-    if(action == newsate[8])  # Zaleti se v rob - je npr. na severu in spet želi proti severu
+    if(action == newsate[9])  # Zaleti se v rob - je npr. na severu in spet želi proti severu
       reward <- reward - 1
     else
       reward <- reward + 3
   }
   
   # HRANA / PIJAČA
+  isThirsty = newstate[13] == 0
+  isHungry = newstate[14] == 0
+  
   if (action == 5) 
   {
-    if(!isPreyHungry(simData, preyId)) # ... čeprav agent ni lačen
+    if(!isHungry && !isThirsty) # ... čeprav agent ni lačen / žejen
       reward <- reward - 3
-    else  # Agent je lačen
+    else  # Agent je lačen / žejen
       reward <- reward + 5
   }
   else
   {
-    if(isPreyThirsty(simData, preyId) && newstate[2] - oldstate[2] < 0) # AGENT ŽEJEN IN BLIŽJE VODI
+    if(isThirsty && newstate[3] - oldstate[3] < 0 || isHungry && newstate[5] - oldstate[5] < 0) # AGENT ŽEJEN / LAČEN IN BLIŽJE VODI / TRAVI
       reward <- reward + 3
-    else if(isPreyThirsty(simData, preyId) && newstate[2] - oldstate[2] > 0)  # AGENT ŽEJEN IN DLJE OD VODE
-      reward <- reward - 3
-    
-    if(isPreyHungryt(simData, preyId) && newstate[4] - oldstate[4] < 0) # AGENT LAČEN IN BLIŽJE TRAVE
-      reward <- reward + 3
-    else if(isPreyHungry(simData, preyId) && newstate[4] - oldstate[4] > 0) # AGENT LAČEN IN DLJE OD TRAVE
+    else if(isThirsty && newstate[3] - oldstate[3] > 0 || isHungry && newstate[5] - oldstate[5] > 0)  # AGENT ŽEJEN / LAČEN IN DLJE OD VODE / TRAVE
       reward <- reward - 3
   }
-  
-  #if(isPreyInForest(simData, preyId))
-  #{
-  #  if(action == 1 && )
-  #}
   
 	reward	
 }
--- skripte/main.R
+++ skripte/main.R
@@ -1,4 +1,6 @@
-source("RL.R")
+setwd("C://Users//kleme//OneDrive//Dokumenti//uiSeminarska2//skripte");
+
+source("customRLs/Klementina_RL1.R");
 
 #
 # Lahko eksperimentirate z razlicnimi nastavitvami simuliranega okolja
@@ -14,7 +16,10 @@ source("RL.R")
 # predstavlja vektor c(30, 4, 5) najvecje vrednosti istoleznih elementov
 # v opisu stanja
 
-qmat <- qlearning(c(30, 4, 5), maxtrials=1000)
+# Distance, direction, border
+#qmat <- qlearning(c(30, 4, 5), maxtrials=1000)
+
+qmat <- qlearning(c(10, 4, 10, 4, 10, 4, 10, 4, 5, 1, 3, 1, 1, 1))
 
 # save(qmat, file="qmat.RData")
 # load(file="qmat.RData")
